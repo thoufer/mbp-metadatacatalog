2e30dbaace88f8b7d3c567466db5f2152d528ac8
Nathan Zimpfer <Thoufer@gmail.com>
2018-02-27 12:46:48 -0500

    Show filter result and record count (Closes #14)
    Bottom of Asset table listing now show the index of the current record
    set being displayed, along with the total record count.
    
    For example, when viewing the full asset table the first page should
    read "Showing 1-25 of 121 results".  When navigating to page 2 is should
    read "Showing 26-50 of 121 results.
    
    If a user filters the result set the 'total row count' returned will be
    the total number of records in the filtered set, not the total number of
    records in the table.

--- asset/utils.py
+++ asset/utils.py
@@ -1,5 +1,5 @@
 from django_tables2 import SingleTableView, RequestConfig
-
+from django.core.paginator import Paginator
 
 class PagedFilteredTableView(SingleTableView):
     """
@@ -17,11 +17,17 @@ class PagedFilteredTableView(SingleTableView):
 
     def get_table(self, **kwargs):
         table = super(PagedFilteredTableView, self).get_table()
-        RequestConfig(self.request, paginate={#'page': self.kwargs['page'],
-                            "per_page": self.paginate_by}).configure(table)
+        RequestConfig(self.request, paginate={'page': self.request.GET.get('page'),
+                        'per_page': self.paginate_by}).configure(table)
         return table
 
     def get_context_data(self, **kwargs):
         context = super(PagedFilteredTableView, self).get_context_data()
         context[self.context_filter_name] = self.filter
+
+        page = self.request.GET.get('page')
+        context['record_start'] = context['paginator'].get_page(page).start_index()
+        context['record_end'] =  context['paginator'].get_page(page).end_index()
+        context['rowcount'] = context['paginator'].count
+
         return context
