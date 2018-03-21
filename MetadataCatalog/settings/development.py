41d53dca9d79babed869b5b5a0cb66c9ef4588cc
Nathan Zimpfer <Thoufer@gmail.com>
2018-03-21 09:38:02 -0400

    Remove unused, commented out settings in Dev.

--- MetadataCatalog/settings/development.py
+++ MetadataCatalog/settings/development.py
@@ -17,18 +17,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': get_env_variable('DB_ENGINE'),
-#         'NAME': get_env_variable('DB_NAME'),
-#         'USER': get_env_variable('DB_USER'),
-#         'PASSWORD': get_env_variable('DB_PASSWORD'),
-#         'HOST': get_env_variable('DB_HOST'),
-#         'PORT': get_env_variable('DB_PORT'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
