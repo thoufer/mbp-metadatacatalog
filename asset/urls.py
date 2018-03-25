7657c0da2fc881df7f5929d86bd5b6c71eff669c
Nathan Zimpfer <Thoufer@gmail.com>
2018-03-25 08:58:32 -0400

    Work around autocomplete bug when new tag created (closes #57)
    The select presents methods to create a new tag using the built in
    select2 method, and a method from dal or taggit [not sure].  If use the
    latter the new tag was repeated 4 times in the select box. Disable new
    tag addition in the autcomplete url.
    
    New tags still appear to be created

--- asset/urls.py
+++ asset/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('detail/<int:pk>/', AssetDetailView.as_view(), name='detail-asset'),
     path('add/', AssetCreateView.as_view(), name='add-asset'),
     path('edit/<int:pk>/', AssetUpdateView.as_view(), name='update-asset'),
-    path('api/subject-autocomplete', SubjectAutocomplete.as_view(create_field='name'), name='get-subject-tags'),
-    path('api/place-autocomplete', PlaceAutocomplete.as_view(create_field='name'), name='get-place-tags'),
+    path('api/subject-autocomplete', SubjectAutocomplete.as_view(), name='get-subject-tags'),
+    path('api/place-autocomplete', PlaceAutocomplete.as_view(), name='get-place-tags'),
     path('api/asset', AssetAPI.as_view({'get':'list'}), name='api-asset'),
 ]
